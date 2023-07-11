b7963b6459bb47735929999c66fa3dd62e82ba20
Raphael Faria <raphael.faria@ribccs.com>
2023-07-11 12:03:09 +0200

    add tailwind button styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './Button';
+import { GoBell } from 'react-icons/go';
+import { BsCloudDownload } from 'react-icons/bs';
+import { GoDatabase } from 'react-icons/go';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello</h1>
+      <div>
+        <Button primary><GoBell />Click here</Button>
+      </div>
+      <div>
+        <Button secondary><BsCloudDownload />Click here</Button>
+      </div>
+      <div>
+        <Button danger><GoDatabase />Click here</Button>
+      </div>
+      <div>
+        <Button success>Click here</Button>
+      </div>
+      <div>
+        <Button warning>Click here</Button>
+      </div>
+      <div>
+        <Button>Click here</Button>
+      </div>
+      <div>
+        <Button primary rounded>Click here</Button>
+      </div>
+      <div>
+        <Button secondary rounded>Click here</Button>
+      </div>
+      <div>
+        <Button danger rounded>Click here</Button>
+      </div>
+      <div>
+        <Button success rounded>Click here</Button>
+      </div>
+      <div>
+        <Button warning rounded>Click here</Button>
+      </div>
+      <div>
+        <Button primary rounded outline>Click here</Button>
+      </div>
+      <div>
+        <Button secondary rounded outline>Click here</Button>
+      </div>
+      <div>
+        <Button danger rounded outline>Click here</Button>
+      </div>
+      <div>
+        <Button success rounded outline>Click here</Button>
+      </div>
+      <div>
+        <Button warning rounded outline>Click here</Button>
+      </div>
     </div>
   );
 }
